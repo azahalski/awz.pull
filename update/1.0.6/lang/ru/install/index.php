cfb576799243ae947d19fb1ecc7cf767699420ff
Andrew Zahalski <andrew@zahalski.dev>
2025-09-10 13:31:00 +0300

    gulp checkpoint

---
+++ update/1.0.6/lang/ru/install/index.php
@@ -0,0 +1,8 @@
+<?
+$MESS['AWZ_PULL_INSTALL_TITLE'] = "Удаление модуля AWZ: Push and Pull";
+$MESS['AWZ_PULL_MODULE_NAME'] = "AWZ: Push and Pull";
+$MESS['AWZ_PULL_MODULE_DESCRIPTION'] = "Модуль для взаимодействия с Push and Pull сервером";
+$MESS['AWZ_PARTNER_NAME'] = "Andrew Zahalski";
+$MESS['MOD_UNINST_SAVE_OPTS'] = "Вы можете сохранить основные настройки модуля";
+$MESS['MOD_UNINST_SAVE_EVENTS_DESC'] = "Вы можете сохранить почтовые шаблоны";
+$MESS['MOD_UNINST_SAVE_OPTS_LABEL'] = "Сохранить настройки";
